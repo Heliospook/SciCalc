905443e9c18123cb966ea3fe4f11421b1cfb84c6
Subhajeet <subhajeetlahiri@gmail.com>
2024-09-24 08:27:23 +0530

    Initial commit with spring stub

---
+++ src/main/java/com/subhajeet/SciCalc/SciCalcApplication.java
@@ -0,0 +1,13 @@
+package com.subhajeet.SciCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SciCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SciCalcApplication.class, args);
+	}
+
+}
